e5c686f148a60963c0b4f753a1b4c04f389128cd
alex02r <alex.giochi02@gmail.com>
2024-01-23 14:11:07 +0100

    [UPGRADE] Add original paragraph

--- request.php
+++ request.php
@@ -12,6 +12,20 @@
         //get value of word
         $word = $_GET['word'];
 
+        $new_p = explode($word, $p);
     ?>
+    <header>
+        <h1>Correct Text</h1>
+    </header>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h3>Paragrafo iniziale</h3>
+                    <p> <?php echo $p; ?> </p>
+                </div>
+            </div>
+        </div>
+    </main>
 </body>
 </html>
